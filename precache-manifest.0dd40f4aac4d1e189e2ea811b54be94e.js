06033d3f3a9ed9159d874d9266a6de30b75c7afa
_MrEasyE_ <mreasye@protonmail.com>
2019-10-31 15:53:11 -0400

    Updates

---
+++ precache-manifest.0dd40f4aac4d1e189e2ea811b54be94e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07bbd81af8267b3941eb02315ce8e024",
+    "url": "/pretty-fizz-buzz/index.html"
+  },
+  {
+    "revision": "7604818f3fa4ca091333",
+    "url": "/pretty-fizz-buzz/static/css/main.9cf0d2ce.chunk.css"
+  },
+  {
+    "revision": "739c0ab08010af62ef82",
+    "url": "/pretty-fizz-buzz/static/js/2.fb563cc1.chunk.js"
+  },
+  {
+    "revision": "7604818f3fa4ca091333",
+    "url": "/pretty-fizz-buzz/static/js/main.5cc45960.chunk.js"
+  },
+  {
+    "revision": "a745abaee5f83ac37807",
+    "url": "/pretty-fizz-buzz/static/js/runtime-main.c48a2170.js"
+  }
+]);
